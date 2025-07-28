10993600f6fe9cfda1456446e3fa588f79788820
Nickson Ogm <nicksonogm@gmail.com>
2025-07-28 15:56:16 +0300

    created a graphql schema

---
+++ server/graphql/schema/user.schema.js
@@ -0,0 +1,47 @@
+const userSchema = `
+type User {
+  id: ID!
+  fullName: String!
+  email: String!
+  phone: String
+  location: String
+  provider: String!
+  avatarUrl: String
+  createdAt: String!
+}
+
+type AuthPayload {
+  token: String!
+  user: User!
+}
+
+input LocalSignupInput {
+  fullName: String!
+  email: String!
+  password: String!
+  phone: String
+  location: String
+}
+
+input GoogleSignupInput {
+  fullName: String!
+  email: String!
+  providerId: String!
+  avatarUrl: String
+}
+
+input LoginInput {
+  email: String!
+  password: String!
+}
+
+type Mutation {
+  localSignup(input: LocalSignupInput!): AuthPayload
+  googleSignup(input: GoogleSignupInput!): AuthPayload
+  login(input: LoginInput!): AuthPayload
+}
+
+type Query {
+  me: User
+}`;
+module.exports = userSchema;
