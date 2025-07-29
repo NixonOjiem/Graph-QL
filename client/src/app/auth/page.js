9390e18bc1060e026a9bac1759dd195317e54caa
Nickson Ogm <nicksonogm@gmail.com>
2025-07-29 10:37:21 +0300

    started workin on frontend

--- client/src/app/auth/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function page() {
-  return <div> authpage</div>;
-}
-
-export default page;
--- client/src/app/components/ApolloProvider.js
+++ client/src/app/components/ApolloProvider.js
@@ -1,5 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
-import { client } from "../lib/apollo-client";
+import { client } from "../../../lib/apollo-client";
 
 export default function ApolloWrapper({ children }) {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
