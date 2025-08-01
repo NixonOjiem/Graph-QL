61c95227e53dc091e16eee90171fa60362f1d1db
Nickson Ogm <nicksonogm@gmail.com>
2025-08-01 17:30:06 +0300

    created country page

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   graphqlEndpoint: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flags.example.com',
+        port: '',
+        pathname: '/**', // Allows all image paths from this hostname
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ client/src/app/country/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingComponent from "../components/LandingComponent";
+
+function page() {
+  return (
+    <div>
+      <LandingComponent />
+    </div>
+  );
+}
+
+export default page;
